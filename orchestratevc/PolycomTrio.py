cc0d67507fa885d1d3ad4dfee0bdf3f335e057eb
Jason Neurohr <Jason.Neurohr@alphawest.com.au>
2017-12-08 14:33:08 +1100

    Modified: PolycomTrio.py, additional methods

--- orchestratevc/PolycomTrio.py
+++ orchestratevc/PolycomTrio.py
@@ -7,6 +7,8 @@ class PolycomTrio:
         self.__address = str(address)
         self.__username = str(username)
         self.__password = str(password)
+        self.__session = requests.session()
+        self.__session.auth = self.get_username(), self.get_password()
     
     def __str__(self):
         return json.dumps({
@@ -34,8 +36,8 @@ class PolycomTrio:
         self.__password = password
 
     def safe_restart(self):
-        session = requests.session()
-        session.auth = self.get_username(), self.get_password()
+        # session = requests.session()
+        # session.auth = self.get_username(), self.get_password()
 
         try:
             response = session.post(
@@ -51,6 +53,110 @@ class PolycomTrio:
                 return "success"
             else:
                 return response.text
+
+        except (requests.ConnectTimeout) as e:
+            return "Connection timeout"
+
+    def network_info(self):
+        session = requests.session()
+        session.auth = self.get_username(), self.get_password()
+
+        try:
+            response = session.get(
+                "https://" + self.get_address() + "/api/v1/mgmt/network/info",
+                headers={"Content-Type": "application/json"},
+                verify=False, # Most Trio deployments use self-signed certificates
+                timeout=2
+            )
+        
+            response_dict = json.loads(response.text)
+
+        except (requests.ConnectTimeout) as e:
+            return "Connection timeout"
+
+    def device_info(self):
+        session = requests.session()
+        session.auth = self.get_username(), self.get_password()
+
+        try:
+            response = session.get(
+                "https://" + self.get_address() + "/api/v1/mgmt/device/info",
+                headers={"Content-Type": "application/json"},
+                verify=False, # Most Trio deployments use self-signed certificates
+                timeout=2
+            )
+        
+            response_dict = json.loads(response.text)
+        
+        except (requests.ConnectTimeout) as e:
+            return "Connection timeout"
+
+    def line_info(self):
+        session = requests.session()
+        session.auth = self.get_username(), self.get_password()
+
+        try:
+            response = session.get(
+                "https://" + self.get_address() + "/api/v1/mgmt/lineInfo",
+                headers={"Content-Type": "application/json"},
+                verify=False, # Most Trio deployments use self-signed certificates
+                timeout=2
+            )
+        
+            response_dict = json.loads(response.text)
+
+        except (requests.ConnectTimeout) as e:
+            return "Connection timeout"
+
+    def network_stats(self):
+        session = requests.session()
+        session.auth = self.get_username(), self.get_password()
+
+        try:
+            response = session.get(
+                "https://" + self.get_address() + "/api/v1/mgmt/network/stats",
+                headers={"Content-Type": "application/json"},
+                verify=False, # Most Trio deployments use self-signed certificates
+                timeout=2
+            )
+        
+            response_dict = json.loads(response.text)
+
+        except (requests.ConnectTimeout) as e:
+            return "Connection timeout"
+    
+    def get_config(self, config_params=[]):
+        data = json.dumps({"data": config_params}) # Create JSON structure
+        session = requests.session()
+        session.auth = self.get_username(), self.get_password()
+
+        try:
+            response = session.post(
+                "https://" + self.get_address() + "/api/v1/mgmt/config/get",
+                headers={"Content-Type": "application/json"},
+                data=data,
+                verify=False, # Most Trio deployments use self-signed certificates
+                timeout=2
+            )
+        
+            return response.json()
+
+        except (requests.ConnectTimeout) as e:
+            return "Connection timeout"
+
+    def get_time(self):
+        session = requests.session()
+        session.auth = self.get_username(), self.get_password()
+
+        try:
+            response = session.get(
+                "https://" + self.get_address() + "/api/v1/mgmt/network/stats",
+                headers={"Content-Type": "application/json"},
+                verify=False, # Most Trio deployments use self-signed certificates
+                timeout=2
+            )
+        
+            return response.headers
 
         except (requests.ConnectTimeout) as e:
             return "Connection timeout"
